1b6b6a78d6fadfb1c0cbb1c8efc757b01c22de82
Rafał Kulawiak <62555963+Snip33x@users.noreply.github.com>
2022-03-21 20:55:45 +0100

    Item ID Explained

--- Assets/GameDev.tv Assets/Scripts/Inventories/Inventory.cs
+++ Assets/GameDev.tv Assets/Scripts/Inventories/Inventory.cs
@@ -141,6 +141,7 @@ namespace GameDevTV.Inventories
             slots = new InventoryItem[inventorySize];
             slots[0] = InventoryItem.GetFromID("71e73607-4bac-4e42-b7d6-5e6f91e92dc4");
             slots[1] = InventoryItem.GetFromID("0aa7c8b8-4796-42aa-89d0-9d100ea67d7b");
+            slots[2] = InventoryItem.GetFromID("81317caa-0163-4309-a429-9ef5b8235678");
         }
 
         /// <summary>
